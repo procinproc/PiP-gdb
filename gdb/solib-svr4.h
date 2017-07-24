2c51e6f7d0e3104c3152f4817eb185efdd8a6d9d
Shougo Matsushita <shougo@soum.co.jp>
2017-07-24 19:01:56 +0900

    Add svr4_check_link_map() prototype

--- gdb/solib-svr4.h
+++ gdb/solib-svr4.h
@@ -84,6 +84,8 @@ extern struct link_map_offsets *svr4_lp64_fetch_link_map_offsets (void);
    SVR4 run time loader.  */
 int svr4_in_dynsym_resolve_code (CORE_ADDR pc);
 
+#ifdef ENABLE_PIP
 extern int svr4_check_link_map (pid_t pid, const char *filename, CORE_ADDR addr);
+#endif
 
 #endif /* solib-svr4.h */
