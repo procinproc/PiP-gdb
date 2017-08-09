4caf8ef1bdf1747ca2bc457a042baaf6de928526
Shougo Matsushita <shougo@soum.co.jp>
2017-08-09 15:45:11 +0900

    PiP root task program, used by pip.exp and threads.exp
    salvaged from /home/shougo/PIP/test/basics/gdb_test.c by soda

---
+++ gdb/testsuite/gdb.pip/gdb_test.c
@@ -0,0 +1,80 @@
+/*
+  * $RIKEN_copyright:$
+  * $PIP_VERSION:$
+  * $PIP_license:$
+*/
+/*
+  * Written by Atsushi HORI <ahori@riken.jp>, 2016
+*/
+
+#include <test.h>
+#include <unistd.h>
+
+int root_exp = 0;
+
+void *thread1(void *args)
+{
+  int inf = 1;
+  while (inf) {
+    sleep(1);
+  }
+  return NULL;
+}
+
+void *thread2(void *args)
+{
+  int inf = 1;
+  while (inf) {
+    sleep(1);
+  }
+  return NULL;
+}
+
+int main( int argc, char **argv ) {
+  int pipid, ntasks;
+  int i;
+  int err;
+  int retval;
+  pthread_t t1, t2;
+
+  ntasks = NTASKS;
+  TESTINT( pip_init( &pipid, &ntasks, NULL, 0 ) );
+
+  pthread_create(&t1, NULL, thread1, (void *)NULL);
+  pthread_create(&t2, NULL, thread2, (void *)NULL);
+
+  if( pipid == PIP_PIPID_ROOT ) {
+    root_exp = 1;
+    char *sleep_argv[] = {
+	    "./gdb.pip/sleep",
+	    NULL,
+    };
+
+    i = 0;
+    pipid = 0;
+    err = pip_spawn( sleep_argv[0], sleep_argv, NULL, i%4, &pipid, NULL, NULL, NULL );
+    if( err ) {
+      fprintf( stderr, "pip_spawn(%d!=%d)=%d !!!!!!\n", i, pipid, err );
+    }
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    DBGF( "calling pip_wait(%d)", i );
+    TESTINT( pip_wait( i, &retval ) );
+    if( retval != i ) {
+      fprintf( stderr, "[PIPID=%d] pip_wait() returns %d ???\n", i, retval );
+    } else {
+      fprintf( stderr, " terminated. OK\n" );
+    }
+    
+    TESTINT( pip_fin() );
+
+  } else {
+    root_exp = 999;
+    fprintf( stderr, "Hello, I am PIPID[%d] ...", pipid );
+    sleep(120);
+    pip_exit( pipid );
+  }
+  return 0;
+}
