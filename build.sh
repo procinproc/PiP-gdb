932e74754fd0bb9b5c06b1d96887d724c558436b
SODA Noriyuki <soda@sra.co.jp>
2018-05-23 19:35:03 +0900

    add --with-glibc-libdir= and --with-pip= options to configure
    this is part of #472 - update the configure script of PiP-gdb to avoid ad-hoc shell script

--- build.sh
+++ build.sh
@@ -1,40 +1,64 @@
 #!/bin/sh
 
-if [ -z $1 ]; then
-    echo $0 "<PIP_INSTALL_DIR> [ <LDLINUX_PATH> [ <GDB_INSTALL_DIR> ] ]"
-    exit 1
-fi
-
-PIP_PREFIX=$1
-dynamic_linker=${PIP_PREFIX}/lib/ld-2.17.so
-
-if [ -n $2 ]; then
-    if [ -x $2 ];
-    then
-	dynamic_linker=$2
-	LDLINUX=-Wl,--dynamic-linker=$2
-    else
-	echo '$2' is not LD-LINUX or unable to find
+case $# in
+0)	echo >&2 Usage: $0 "--prefix=<DIR> [--with-pip=<PIP_DIR> --with-glibc-libdir=<GLIBC_LIBDIR>]"
 	exit 1
-    fi
-fi
+	;;
+esac
 
-if [ -n $3 ]; then
-    PREFIX="--prefix=$3"
-fi
+: ${BUILD_PARALLELISM=`getconf _NPROCESSORS_ONLN`}
 
 make clean
 make distclean
 find . -name config.cache -delete
 
-./configure --enable-gdb-build-warnings=,-Wno-unused --enable-werror --with-separate-debug-dir=/usr/lib/debug --disable-sim --disable-rpath --with-system-readline --with-expat --without-libexpat-prefix --enable-tui --with-rpm --with-lzma --without-libunwind --enable-64-bit-bfd --enable-inprocess-agent '--with-auto-load-dir=$debugdir:$datadir/auto-load' '--with-auto-load-safe-path=$debugdir:$datadir/auto-load:/usr/bin/mono-gdb.py' --enable-targets=s390-linux-gnu,powerpc-linux-gnu,powerpcle-linux-gnu x86_64-redhat-linux-gnu ${PREFIX}
+./configure \
+	--enable-gdb-build-warnings=,-Wno-unused \
+	--enable-werror \
+	--with-separate-debug-dir=/usr/lib/debug \
+	--disable-sim \
+	--disable-rpath \
+	--with-system-readline \
+	--with-expat \
+	--without-libexpat-prefix \
+	--enable-tui \
+	--with-rpm \
+	--with-lzma \
+	--without-libunwind \
+	--enable-64-bit-bfd \
+	--enable-inprocess-agent \
+	--with-auto-load-dir='$debugdir:$datadir/auto-load' \
+	--with-auto-load-safe-path='$debugdir:$datadir/auto-load:/usr/bin/mono-gdb.py' \
+	--enable-targets=s390-linux-gnu,powerpc-linux-gnu,powerpcle-linux-gnu \
+	"$@" \
+	x86_64-redhat-linux-gnu \
+    &&
 
- make -j8 "CFLAGS=-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong --param=ssp-buffer-size=4 -grecord-gcc-switches -m64 -mtune=generic -DENABLE_PIP -I${PIP_PREFIX}/include" "LDFLAGS=-Wl,-z,relro ${LDLINUX} -Wl,-rpath=${PIP_PREFIX}/lib" maybe-configure-gdb
+make -j ${BUILD_PARALLELISM} "CFLAGS=-O2 -g -pipe -Wall \
+	-Wp,-D_FORTIFY_SOURCE=2 \
+	-fexceptions \
+	-fstack-protector-strong \
+	--param=ssp-buffer-size=4 \
+	-grecord-gcc-switches \
+	-m64 \
+	-mtune=generic" \
+	"LDFLAGS=-Wl,-z,relro" \
+	maybe-configure-gdb \
+    &&
 
-perl -i.relocatable -pe 's/^(D\[".*_RELOCATABLE"\]=" )1(")$/${1}0$2/' gdb/config.status
+perl -i.relocatable -pe 's/^(D\[".*_RELOCATABLE"\]=" )1(")$/${1}0$2/' \
+	gdb/config.status \
+    &&
 
-make -j8 "CFLAGS=-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong --param=ssp-buffer-size=4 -grecord-gcc-switches -m64 -mtune=generic -DENABLE_PIP -I${PIP_PREFIX}/include" "LDFLAGS=-Wl,-z,relro -Wl,--dynamic-linker=$dynamic_linker -Wl,-rpath=${PIP_PREFIX}/lib"
+make -j ${BUILD_PARALLELISM} "CFLAGS=-O2 -g -pipe -Wall \
+	-Wp,-D_FORTIFY_SOURCE=2 \
+	-fexceptions \
+	-fstack-protector-strong \
+	--param=ssp-buffer-size=4 \
+	-grecord-gcc-switches \
+	-m64 \
+	-mtune=generic" \
+	"LDFLAGS=-Wl,-z,relro" \
+    &&
 
-if [ -n ${PREFIX} ]; then
 make install
-fi
